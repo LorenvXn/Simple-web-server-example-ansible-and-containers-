a3ee3331e6b7b607f0de269233b5170819cd09af
Loren vXn <predark.dante@gmail.com>
2018-08-23 17:37:21 +0300

    Update run_container_kafka.sh

--- Real case scenarios/Deploy Big Data env/Kafka/run_container_kafka.sh
+++ Real case scenarios/Deploy Big Data env/Kafka/run_container_kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # keeping it as a prototype for now
-# needs more automation 
+# needs more automation for those <>  values ... why make other's people easier for noW!? hue hue hue
  
 
 docker create -i -t --name <kafka_container> \
