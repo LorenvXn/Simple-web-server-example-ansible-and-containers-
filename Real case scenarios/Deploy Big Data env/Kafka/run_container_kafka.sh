9887b47b55e666571b5855ce61311f381b80660c
Loren vXn <predark.dante@gmail.com>
2018-08-23 17:35:00 +0300

    Update run_container_kafka.sh

--- Real case scenarios/Deploy Big Data env/Kafka/run_container_kafka.sh
+++ Real case scenarios/Deploy Big Data env/Kafka/run_container_kafka.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-
-
 docker create -i -t --name <kafka_container> \
               -p 9092:9092 --cap-add=ALL \
                `docker images | grep <tag> | awk {'print $3'} | grep -vi image` /bin/bash
